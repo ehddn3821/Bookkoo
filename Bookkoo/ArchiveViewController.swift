85b39245efb2f3c4bbcd9060f34484900bf75de7
dwkang <ehddn3821@naver.com>
2021-03-28 16:05:51 +0900

    #1 기본 구조 생성 및 탭바 추가

---
+++ Bookkoo/ArchiveViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bookkoo
+//
+//  Created by dwKang on 2021/03/28.
+//
+
+import UIKit
+
+class ArchiveViewController: UIViewController {
+
+    @IBOutlet var archiveTableView: UITableView!
+    @IBOutlet var readCountLabel: UILabel!
+    @IBOutlet var reviewCountLabel: UILabel!
+    @IBOutlet var reserveCountLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
---
+++ Bookkoo/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  Bookkoo
+//
+//  Created by dwKang on 2021/03/28.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet var bookSearchBar: UISearchBar!
+    @IBOutlet var searchTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
