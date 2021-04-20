3b87cd322396a69a1d3847d8da9d3f0473a8d556
dwkang <ehddn3821@naver.com>
2021-04-20 10:28:34 +0900

    #10 보관함 뷰 수정 및 테이블뷰 추가

--- Bookkoo/ArchiveViewController.swift
+++ Bookkoo/ArchiveViewController.swift
@@ -6,20 +6,104 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ArchiveViewController: UIViewController {
+    
+    let realm = try! Realm()
+    var reviewList: Results<BookModel>!
+    var likeList: Results<BookModel>!
 
     @IBOutlet var archiveTableView: UITableView!
-    @IBOutlet var readCountLabel: UILabel!
     @IBOutlet var reviewCountLabel: UILabel!
-    @IBOutlet var reserveCountLabel: UILabel!
+    @IBOutlet var likeCountLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        titleLabel.layer.addBorder([.top], color: .black, width: 0.5)
         
+        archiveTableView.delegate = self
+        archiveTableView.dataSource = self
+        
+        let nibName = UINib(nibName: "SearchTableViewCell", bundle: nil)
+        archiveTableView.register(nibName, forCellReuseIdentifier: "SearchTableViewCell")
+        
+        // 용도에 맞게 리스트로 추출
+        likeList = realm.objects(BookModel.self).filter("bkLike == true")
+        reviewList = realm.objects(BookModel.self).filter("bkReview != nil")
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // 리뷰 작성한 책 수
+        reviewCountLabel.text = "\(reviewList.count)"
+        
+        // 찜한 책 수
+        likeCountLabel.text = "\(likeList.count)"
+        
+        archiveTableView.reloadData()
+    }
 }
 
+extension ArchiveViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return likeList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath) as! SearchTableViewCell
+        
+        cell.bookAuthor.text = likeList[indexPath.row].bkAuthors
+        
+        cell.bookTitle.text = likeList[indexPath.row].bkTitle
+        
+        if likeList[indexPath.row].bkTranslators != "" {
+            cell.translatorLabel.isHidden = false
+            cell.bookTranslator.isHidden = false
+            cell.bookTranslator.text = likeList[indexPath.row].bkTranslators
+        } else {
+            cell.translatorLabel.isHidden = true
+            cell.bookTranslator.isHidden = true
+        }
+        
+        let url = URL(string: likeList[indexPath.row].bkThumbnail)
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!)
+            DispatchQueue.main.async {
+                cell.bookImage.image = UIImage(data: data!)
+            }
+        }
+        
+        return cell
+    }
+}
+
+// 테두리 원하는 방향만
+extension CALayer {
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
+    }
+}
